184443c064fa9aeda0ac73e6d754a27ecc9ddc9b
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-17 11:06:00 -0400

    part b q3 done

--- Homework/Homework 1/al7814_hw1_q3.py
+++ Homework/Homework 1/al7814_hw1_q3.py
@@ -9,5 +9,14 @@ def sum_under_square(n):
     
     return total
 
+# part b
+
+def gen_next_square(n):
+    base = 1
+    while base ** 2 < n:
+        yield base ** 2
+        base += 1
+
 if __name__ == "__main__":
     print(sum_under_square(17))
+    print(sum([x for x in gen_next_square(17)]))
