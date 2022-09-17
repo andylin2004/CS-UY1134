9a37cebbdcb7db620fde0510efbac0d99834b494
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-17 11:08:46 -0400

    part c q3 done w test case

--- Homework/Homework 1/al7814_hw1_q3.py
+++ Homework/Homework 1/al7814_hw1_q3.py
@@ -17,6 +17,18 @@ def gen_next_square(n):
         yield base ** 2
         base += 1
 
+# part c
+
+def sum_under_odd_square(n):
+    total = 0
+    base = 1
+    while base ** 2 < n:
+        total += base ** 2
+        base += 2
+    
+    return total
+
 if __name__ == "__main__":
     print(sum_under_square(17))
     print(sum([x for x in gen_next_square(17)]))
+    print(sum_under_odd_square(17))
