fc1a8dc38324e47d35152d5cc3355f5858e2a238
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-17 01:20:23 -0400

    part b done

--- Homework/Homework 1/al7814_hw1_q4.py
+++ Homework/Homework 1/al7814_hw1_q4.py
@@ -3,6 +3,11 @@
 powers = [10 ** x for x in range(6)]
 print(powers)
 
+# part b
+
+sequence = [x * (x+1) for x in range(10)]
+print(sequence)
+
 # part c
 letters = [chr(x) for x in range(97,123)]
 print(letters)
