8457f7c6227ae296ff5294db18f13e4d339ff10d
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-17 11:18:39 -0400

    q5 done

---
+++ Homework/Homework 1/al7814_hw1_q5.py
@@ -0,0 +1,17 @@
+def fibs(n):
+    last = 1
+    second_to_last = 1
+
+    for i in range(n):
+        if i == 0:
+            yield last
+        else:
+            if i == 1:
+                yield second_to_last
+            else:
+                yield last + second_to_last
+                temp = last
+                last = second_to_last
+                second_to_last += temp
+
+print([x for x in fibs(8)])
