75bc325a43ea2cbeeb55b61e4bd752b6c1729442
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-18 13:47:32 -0400

    right shifting works

--- Homework/Homework 1/al7814_hw1_q2.py
+++ Homework/Homework 1/al7814_hw1_q2.py
@@ -1,12 +1,22 @@
-def shift(lst, k):
-    for _ in range(k):
-        tmp = lst[0]
-        for i in range(1, len(lst)):
-            lst[i-1] = lst[i]
-        lst[len(lst)-1] = tmp
+def shift(lst, k, shift_direction = "left"):
+    if shift_direction == "left":
+        for _ in range(k):
+            tmp = lst[0]
+            for i in range(1, len(lst)):
+                lst[i-1] = lst[i]
+            lst[len(lst)-1] = tmp
+    elif shift_direction == "right":
+        for _ in range(k):
+            tmp = lst[len(lst) - 1]
+            for i in range(len(lst) - 2, -1, -1):
+                lst[i+1] = lst[i]
+            lst[0] = tmp
+
 
 if __name__ == "__main__":
     lst = [1,2,3,4,5,6]
     print(lst)
     shift(lst, 2)
+    print(lst)
+    shift(lst, 2, "right")
     print(lst)
