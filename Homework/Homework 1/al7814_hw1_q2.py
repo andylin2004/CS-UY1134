826c8ca41bdf8caa732abbabff20cb02ef26b23e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-18 13:00:18 -0400

    added test case for q2

--- Homework/Homework 1/al7814_hw1_q2.py
+++ Homework/Homework 1/al7814_hw1_q2.py
@@ -4,3 +4,9 @@ def shift(lst, k):
         for i in range(1, len(lst)):
             lst[i-1] = lst[i]
         lst[len(lst)-1] = tmp
+
+if __name__ == "__main__":
+    lst = [1,2,3,4,5,6]
+    print(lst)
+    shift(lst, 2)
+    print(lst)
