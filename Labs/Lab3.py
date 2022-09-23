43c6ff4bcab58d4ffb88add25a2b0c21b13964fe
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-23 09:32:56 -0400

    part b done

--- Labs/Lab3.py
+++ Labs/Lab3.py
@@ -13,3 +13,23 @@ def reverse_list(lst):
 list = [1,2,3,4,5,6]
 reverse_list(list)
 print(list)
+
+# part b
+def reverse_list(lst, low = None, high = None):
+    """
+    : lst type: list[]
+    : low, high type: int
+    : return type: None
+    """
+    if low == high == None:
+        low = 0
+        high = len(lst) - 1
+    
+    for i in range(low, ((high + 1 - low) // 2) + low):
+        tmp = lst[i]
+        lst[i] = lst[high+1-i]
+        lst[high+1-i] = tmp
+
+list = [1,2,3,4,5,6]
+reverse_list(list, 1, 3)
+print(list)
