aaab7fd92fb8f37427037d095c07c871ce15b310
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-23 10:54:55 -0400

    tryhard q2

--- Labs/Lab3.py
+++ Labs/Lab3.py
@@ -33,3 +33,31 @@ def reverse_list(lst, low = None, high = None):
 list = [1,2,3,4,5,6]
 reverse_list(list, 1, 3)
 print(list)
+
+#question 2
+#part a
+
+def move_zeros(nums): 
+    """
+    : nums type: list[int]
+    : return type: None
+    """
+
+    zeroToMove = -1
+    i = 0
+    while i < len(nums):
+        print(i)
+        if nums[i] != 0 and zeroToMove != -1 and zeroToMove < i:
+                nums[zeroToMove] = nums[i]
+                nums[i] = 0
+                tmp = zeroToMove
+                zeroToMove = i
+                i = tmp
+        elif zeroToMove > i and nums[i] == 0:
+            zeroToMove = i
+            i-=1
+        i += 1
+
+list = [0, 1, 0, 3, 13, 0]
+move_zeros(list)
+print(list)
