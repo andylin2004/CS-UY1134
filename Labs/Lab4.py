15e214041f0a47a877cc23d7ae6f21573f78ebf0
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-09-30 10:02:38 -0400

    q2 done

--- Labs/Lab4.py
+++ Labs/Lab4.py
@@ -12,3 +12,30 @@ def is_palindrome(s):
     return True
 
 print(is_palindrome("55055"))
+
+# question 2
+
+def is_vovel(char):
+    return char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u'
+
+def reverse_vowels(input_str):
+    """
+    : input_str type: string
+    : return type: string
+    """
+    list_str = list(input_str) #list constructor guarantees Theta(n)
+    # Your code implementation goes here
+    left = 0
+    right = len(list_str)-1
+    while (left < len(list_str) or right >= 0) and left < right:
+        if not is_vovel(list_str[left]):
+            left += 1
+        if not is_vovel(list_str[right]):
+            right -= 1
+        if is_vovel(list_str[left]) and is_vovel(list_str[right]):
+            (list_str[left], list_str[right]) = (list_str[right], list_str[left])
+            left += 1
+            right -= 1
+    return "".join(list_str)
+
+print(reverse_vowels("agar.io"))
