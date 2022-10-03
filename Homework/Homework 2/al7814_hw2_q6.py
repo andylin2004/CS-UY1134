064c7b67197cc9f5dca1fc21dcac3c8f15c24aef
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-03 18:45:45 -0400

    initial attempt at a6

--- Homework/Homework 2/al7814_hw2_q6.py
+++ Homework/Homework 2/al7814_hw2_q6.py
@@ -1,2 +1,18 @@
 def two_sum(srt_lst, target):
-    pass
+    for i in range(len(srt_lst)):
+        compliment = target - srt_lst[i]
+        left = 0
+        right = len(srt_lst) - 1
+        while right - left > 1:
+            mid = (left + right) // 2
+            if srt_lst[mid] > compliment:
+                right = mid
+            elif srt_lst[mid] < compliment:
+                left = mid
+        if srt_lst[left] == compliment:
+            return (i, left)
+        elif srt_lst[right] == compliment:
+            return (i, right)
+
+if __name__ == "__main__":
+    print(two_sum([-2, 7, 11, 15, 20, 21], 22))
