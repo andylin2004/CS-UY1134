f93dc7c601666e7802de19c71835913d050ca62f
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-03 15:34:54 -0400

    finished q3

--- Homework/Homework 2/al7814_hw2_q3.py
+++ Homework/Homework 2/al7814_hw2_q3.py
@@ -4,6 +4,9 @@ def factors(num):
     for i in range(1, round(sqrt(num)) + 1):
         if num % i == 0:
             yield i
+    for i in range(round(sqrt(num)) + 1, 0, -1):
+        if num % i == 0 and num // i != i:
+            yield num // i
 
 for curr_factor in factors(100):
     print(curr_factor)
