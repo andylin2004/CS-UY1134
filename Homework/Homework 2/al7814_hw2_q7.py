6fbd459d27a5347ed0d5391fb8152cf1c0639a8e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-03 18:26:31 -0400

    deal with edge cases

--- Homework/Homework 2/al7814_hw2_q7.py
+++ Homework/Homework 2/al7814_hw2_q7.py
@@ -12,8 +12,10 @@ def findChange(lst01):
                 return None
         if lst01[left] == lst01[mid] == 0:
             left = mid + 1
-        elif lst01[left] == 0 and lst01[mid] == 1:
+        elif lst01[mid] == 1:
             right = mid
+        else:
+            return None
 
 if __name__ == "__main__":
-    print(findChange([0, 0, 0, 0, 1, 1, 1, 1]))
+    print(findChange([0, 0, 0, 1, 1, 1, 1, 1]))
