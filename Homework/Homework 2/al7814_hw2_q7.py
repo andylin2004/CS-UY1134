ed8bdb625bdb903812259b8bd049f63577a0ffbc
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-03 17:09:11 -0400

    q7 done

---
+++ Homework/Homework 2/al7814_hw2_q7.py
@@ -0,0 +1,19 @@
+def findChange(lst01):
+    left = 0
+    right = len(lst01) - 1
+    while True:
+        mid = (left + right) // 2
+        if right - left == 1:
+            if lst01[left] == 1:
+                return left
+            if lst01[right] == 1:
+                return right
+            else:
+                return None
+        if lst01[left] == lst01[mid] == 0:
+            left = mid + 1
+        elif lst01[left] == 0 and lst01[mid] == 1:
+            right = mid
+
+if __name__ == "__main__":
+    print(findChange([0, 0, 0, 0, 1, 1, 1, 1]))
