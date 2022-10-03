219e46c0341d9a9ccd29959f4c3627aa14b33507
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-03 15:56:09 -0400

    main for q4

--- Homework/Homework 2/al7814_hw2_q4.py
+++ Homework/Homework 2/al7814_hw2_q4.py
@@ -5,3 +5,13 @@ def e_approx(n):
         if i != 0:
             denominator *= i
         total += 1 / denominator
+    return total
+
+if __name__ == "__main__":
+    def main():
+        for n in range(15):
+            curr_approx = e_approx(n)
+            approx_str = "{:.15f}".format(curr_approx)
+            print("n =", n, "Approximation:", approx_str)
+    
+    main()
