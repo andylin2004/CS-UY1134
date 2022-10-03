6344f8187f24301ef16413860026a3770883f776
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-03 19:09:14 -0400

    adjusted q6 code

--- Homework/Homework 2/al7814_hw2_q6.py
+++ Homework/Homework 2/al7814_hw2_q6.py
@@ -5,14 +5,18 @@ def two_sum(srt_lst, target):
         right = len(srt_lst) - 1
         while right - left > 1:
             mid = (left + right) // 2
+            print(compliment, left, mid, right)
             if srt_lst[mid] > compliment:
                 right = mid
             elif srt_lst[mid] < compliment:
                 left = mid
+            elif srt_lst[mid] == compliment:
+                return (i, mid)
         if srt_lst[left] == compliment:
             return (i, left)
         elif srt_lst[right] == compliment:
             return (i, right)
+    return None
 
 if __name__ == "__main__":
-    print(two_sum([-2, 7, 11, 15, 20, 21], 22))
+    print(two_sum([-2, 7, 11, 15, 20, 21,22], 33))
