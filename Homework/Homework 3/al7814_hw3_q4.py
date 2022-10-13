abf705c092e702aeee702236ac66b56f799b466a
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-13 00:03:00 -0400

    delete elements that wont be useful anymore

--- Homework/Homework 3/al7814_hw3_q4.py
+++ Homework/Homework 3/al7814_hw3_q4.py
@@ -1,11 +1,15 @@
 def removeAll(lst, item):
     lookAheadRange = 0
     i = 0
-    while i < len(lst) - lookAheadRange:
-        if lst[i+lookAheadRange] == item:
-            lookAheadRange += 1
+    while i < len(lst):
+        if i < len(lst) - lookAheadRange:
+            if lst[i+lookAheadRange] == item:
+                lookAheadRange += 1
+            else:
+                lst[i] = lst[i+lookAheadRange]
+                i += 1
         else:
-            lst[i] = lst[i+lookAheadRange]
+            lst[i] = None
             i += 1
     
 if __name__ == "__main__":
