d03e6e622e8e45dd8c0fe0c8c708573df5c32832
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-13 00:01:32 -0400

    fix issue where imported code did not account for the cut length

--- Homework/Homework 3/al7814_hw3_q4.py
+++ Homework/Homework 3/al7814_hw3_q4.py
@@ -1,9 +1,14 @@
 def removeAll(lst, item):
     lookAheadRange = 0
     i = 0
-    while i < len(lst):
+    while i < len(lst) - lookAheadRange:
         if lst[i+lookAheadRange] == item:
             lookAheadRange += 1
         else:
             lst[i] = lst[i+lookAheadRange]
             i += 1
+    
+if __name__ == "__main__":
+    lst = [2,1,2,3,2,3,4,2,2]
+    removeAll(lst, 2)
+    print(lst)
