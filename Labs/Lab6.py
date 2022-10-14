6509447bfc0133b3dbf4c77ee839366032a7741e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-14 09:33:34 -0400

    q2 done, slight mod to q1

--- Labs/Lab6.py
+++ Labs/Lab6.py
@@ -1,9 +1,18 @@
 #q1
 def sum_to(n):
-    if n == 1:
+    if n <= 1:
         return 1
     else:
         return n + sum_to(n-1)
 
 print(sum_to(16))
 print(sum([x for x in range(17)]))
+
+#q2
+def product_evens(n):
+    if n <= 2:
+        return 2
+    else:
+        return n * product_evens(n-2)
+
+print(product_evens(8))
