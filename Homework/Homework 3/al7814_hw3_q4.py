276e7d844466f5f49edd39f3df29ab4c35554d95
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-15 16:52:31 -0400

    adjust q4 codebase

--- Homework/Homework 3/al7814_hw3_q4.py
+++ Homework/Homework 3/al7814_hw3_q4.py
@@ -1,18 +1,16 @@
 def remove_all(lst, item):
     lookAheadRange = 0
     i = 0
-    while i < len(lst):
-        if i < len(lst) - lookAheadRange:
-            if lst[i+lookAheadRange] == item:
-                lookAheadRange += 1
-            else:
-                lst[i] = lst[i+lookAheadRange]
-                i += 1
+    while i < len(lst) - lookAheadRange:
+        if lst[i+lookAheadRange] == item:
+            lookAheadRange += 1
         else:
-            lst[i] = None
+            lst[i] = lst[i+lookAheadRange]
             i += 1
+    for _ in range(lookAheadRange):
+        lst.pop()
     
 if __name__ == "__main__":
     lst = [2,1,2,3,2,3,4,2,2]
-    removeAll(lst, 2)
+    remove_all(lst, 2)
     print(lst)
