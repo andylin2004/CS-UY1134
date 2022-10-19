4fd0a9730f5d9eafba416830bd21c4df07e60d2b
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-19 11:39:57 -0400

    part b done

--- Homework/Homework 4/al7814_hw4_q3.py
+++ Homework/Homework 4/al7814_hw4_q3.py
@@ -1,3 +1,5 @@
+#part a
+
 def print_triangle(n):
     print_triangle_recursive(1, n)
 
@@ -7,5 +9,18 @@ def print_triangle_recursive(cur, max):
         print_triangle_recursive(cur + 1, max)
 
 
+#part b
+
+def print_decreasing_triangle_recursive(n):
+    print("*" * n)
+    if n > 1:
+        print_decreasing_triangle_recursive(n - 1)
+
+def print_oposite_triangle(n):
+    print_decreasing_triangle_recursive(n)
+    print_triangle(n)
+
 if __name__ == "__main__": 
     print_triangle(4)
+    print()
+    print_oposite_triangle(4)
