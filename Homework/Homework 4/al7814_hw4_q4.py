866ef4723cb60d32f41022bd240679293e52ceff
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-20 21:40:21 -0400

    main func for testing added

--- Homework/Homework 4/al7814_hw4_q4.py
+++ Homework/Homework 4/al7814_hw4_q4.py
@@ -6,3 +6,9 @@ def list_min(lst, low, high):
             return list_min(lst, low+1, high)
         else:
             return list_min(lst, low, high-1)
+
+if __name__ == "__main__": 
+    lst = [1,2,3,4,5,6]
+    print(list_min(lst, 0, len(lst) - 1))
+    lst = [2,3,1,1,4,5,6,0]
+    print(list_min(lst, 0, len(lst) - 1))
