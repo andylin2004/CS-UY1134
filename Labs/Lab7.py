d47d28aa57fc28bacd228311dbd5baa5704db53c
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-22 18:42:20 -0400

    q1 done

--- Labs/Lab7.py
+++ Labs/Lab7.py
@@ -6,18 +6,17 @@ def split_parity(lst, low, high):
     : low, high type: int
     : return type: None
     """
-    mid = (low+high)//2
-    i1 = low
-    i2 = mid
     if high - low > 0:
-        split_parity(lst, low, mid)
-        split_parity(lst, mid, high)
-    while i1 < mid and i2 < high:
-        if lst[i1] % 2 == 0 and lst[i2] % 2 == 1:
-            (lst[i1], lst[i2]) = (lst[i2], lst[i1])
-        else:
-            if lst[i1] % 2 == 0:
-                i1 += 1
-            if lst[i2] % 2 == 1:
-                i2 += 1
+        if lst[low] % 2 == 1 and lst[high] % 2 == 0:
+            (lst[low], lst[high]) = (lst[high], lst[low])
+            split_parity(lst,low+1,high-1)
+        elif lst[low] % 2 == 0:
+            split_parity(lst,low+1,high)
+        elif lst[high] % 2 == 1:
+            split_parity(lst,low,high-1)
+
         
+if __name__ == "__main__": 
+    lst = [4,-5,2,3,-1,-6,7,9,0]
+    split_parity(lst, 0, len(lst) - 1)
+    print(lst)
