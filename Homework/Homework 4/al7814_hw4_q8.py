3db9888d48b86a5fc310c29efce97657ef85f691
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-26 00:58:22 -0400

    adjust spacing

--- Homework/Homework 4/al7814_hw4_q8.py
+++ Homework/Homework 4/al7814_hw4_q8.py
@@ -8,7 +8,6 @@ def flat_list(nested_lst, low, high):
     elif isinstance(nested_lst[high], int):
         return_lst.append(nested_lst[high])
     return return_lst
-        
 
 if __name__ == "__main__": 
     nested_lst=[[1, 2], 3, [4, [5, 6, [7], 8]]]
