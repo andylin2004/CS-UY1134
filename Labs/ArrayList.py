729f888ffcba9d5bb3da25136422ae0487dedaf4
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-28 09:09:55 -0400

    got new arraylist to fix broken component

--- Labs/ArrayList.py
+++ Labs/ArrayList.py
@@ -4,10 +4,9 @@ def make_array(n):
 
 class ArrayList:
     def __init__(self):
-        self.data_arr = make_array(1)
+        self.data = make_array(1)
         self.capacity = 1
         self.n = 0
-
 
     def __len__(self):
         return self.n
@@ -16,35 +15,81 @@ class ArrayList:
     def append(self, val):
         if (self.n == self.capacity):
             self.resize(2 * self.capacity)
-        self.data_arr[self.n] = val
+        self.data[self.n] = val
         self.n += 1
-
 
     def resize(self, new_size):
         new_array = make_array(new_size)
         for i in range(self.n):
-            new_array[i] = self.data_arr[i]
-        self.data_arr = new_array
+            new_array[i] = self.data[i]
+        self.data = new_array
         self.capacity = new_size
-
-
-    def __getitem__(self, ind):
-        if (not (0 <= ind <= self.n - 1)):
-            raise IndexError('invalid index')
-        return self.data_arr[ind]
-
-
-    def __setitem__(self, ind, val):
-        if (not (0 <= ind <= self.n - 1)):
-            raise IndexError('invalid index')
-        self.data_arr[ind] = val
-
-
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self.data_arr[i]  #could also yield self[i]
-
 
     def extend(self, iter_collection):
         for elem in iter_collection:
             self.append(elem)
+
+
+    def __getitem__(self, ind):
+        if (not (-self.n <= ind <= self.n - 1)):
+            raise IndexError('invalid index')
+        if (ind < 0):
+            ind = self.n + ind
+        return self.data[ind]
+
+    def __setitem__(self, ind, val):
+        if (not (-self.n <= ind <= self.n - 1)):
+            raise IndexError('invalid index')
+        if (ind < 0):
+            ind = self.n + ind
+        self.data[ind] = val
+
+
+    def pop(self, ind = -1):
+        if (not (-self.n <= ind <= self.n - 1)):
+            raise IndexError('invalid index')
+        if (ind < 0):
+            ind = self.n + ind
+        elem = self.data[ind]
+        for i in range(ind+1, self.n):
+            self.data[i-1] = self.data[i]
+        self.data[self.n - 1] = None
+        self.n -= 1
+        if (self.n < self.capacity // 4):
+            self.resize(self.capacity // 2)
+        return elem
+
+    def insert(self, ind, value):
+        if (not (-self.n <= ind <= self.n - 1)):
+            raise IndexError('invalid index')
+        if (ind < 0):
+            ind = self.n + ind
+        if (self.n == self.capacity):
+            self.resize(2 * self.capacity)
+        for j in range(self.n, ind, -1):
+            self.data[j] = self.data[j - 1]
+        self.data[ind] = value
+        self.n += 1
+
+    def __repr__(self):
+        data_as_strings = [str(self.data[i]) for i in range(self.n)]
+        return '[' + ', '.join(data_as_strings) + ']'
+
+    def __add__(self, other):
+        res = ArrayList()
+        res.extend(self)
+        res.extend(other)
+        return res
+
+    def __iadd__(self, other):
+        self.extend(other)
+        return self
+
+    def __mul__(self, times):
+        res = ArrayList()
+        for i in range(times):
+            res.extend(self)
+        return res
+
+    def __rmul__(self, times):
+        return self * times
