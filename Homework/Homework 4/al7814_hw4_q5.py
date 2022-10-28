6a867838d371b20dc8b2acbedf786c7283fa32ae
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-27 21:36:07 -0400

    fix bug where i didnt check base case correctly

--- Homework/Homework 4/al7814_hw4_q5.py
+++ Homework/Homework 4/al7814_hw4_q5.py
@@ -12,7 +12,7 @@ def count_lowercase(s, low, high):
 
 def is_number_of_lowercase_even(s, low, high):
     if low == high:
-        return True
+        return not 97 <= ord(s[low]) < 123
     else:
         if 97 <= ord(s[low]) < 123:
             return not is_number_of_lowercase_even(s, low+1, high)
