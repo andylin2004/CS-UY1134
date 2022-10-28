52135ed8207e469410d9aad1fd09fa97fd90dd36
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-28 09:08:48 -0400

    q2 done

--- Labs/Lab8.py
+++ Labs/Lab8.py
@@ -9,3 +9,20 @@ def stack_sum(s):
         to_return = val + stack_sum(s)
         s.push(val)
         return to_return
+
+#q2
+def eval_prefix(exp_str):
+    exp_lst = exp_str.split(" ")
+    s = ArrayStack()
+    for i in range(len(exp_lst), -1, -1):
+        if exp_lst[i].isdigit():
+            s.push(int(exp_lst[i]))
+        elif exp_lst[i] == "+":
+            s.push(s.pop() + s.pop())
+        elif exp_lst[i] == "-":
+            s.push(s.pop() - s.pop())
+        elif exp_lst[i] == "*":
+            s.push(s.pop() * s.pop())
+        elif exp_lst[i] == "/":
+            s.push(s.pop() / s.pop())
+    return s.top
