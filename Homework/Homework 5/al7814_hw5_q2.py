ac1254a7a6a9af31a6258b0eef023e4b7464b082
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-05 13:36:18 -0400

    add push, top, pop

--- Homework/Homework 5/al7814_hw5_q2.py
+++ Homework/Homework 5/al7814_hw5_q2.py
@@ -10,3 +10,15 @@ class MaxStack:
     
     def __len__(self):
         return len(self.stack)
+    
+    def push(self, e):
+        self.stack.push(e)
+    
+    def top(self):
+        return self.stack.top()
+    
+    def pop(self):
+        if len(self.stack) == 0:
+            raise Exception("Stack is empty")
+        else:
+            return self.stack.pop()
