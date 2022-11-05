85bb510b7b9803ad97c8f2b1f249ac68ed65a9b2
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-05 13:28:08 -0400

    gitignore buffed and better calculator

--- Homework/Homework 5/al7814_hw5_q1.py
+++ Homework/Homework 5/al7814_hw5_q1.py
@@ -7,17 +7,29 @@ while running:
     num_stack = ArrayStack()
     var_name_to_save = ''
     equation_parts = input("--> ").split()
-    for i in equation_parts:
-        if i == '+':
-            num_stack.push(num_stack.pop() + num_stack.pop())
-        elif i == '-':
-            num_stack.push(num_stack.pop() - num_stack.pop())
-        elif i == '*':
-            num_stack.push(num_stack.pop() * num_stack.pop())
-        elif i == '/':
-            num_stack.push(num_stack.pop() / num_stack.pop())
-        elif i == '=':
-            var_name_to_save = equation_parts[0]
-        elif i.isnumeric():
-            num_stack.push(int(i))
+    if len(equation_parts) == 1 and equation_parts[0] == "done()":
+        running = False
+    else:
+        for i in equation_parts:
+            if i == '+':
+                num_stack.push(num_stack.pop() + num_stack.pop())
+            elif i == '-':
+                num_stack.push(- num_stack.pop() + num_stack.pop())
+            elif i == '*':
+                num_stack.push(num_stack.pop() * num_stack.pop())
+            elif i == '/':
+                num_stack.push(1 / num_stack.pop() * num_stack.pop())
+            elif i == '=':
+                var_name_to_save = equation_parts[0]
+            elif i.isnumeric():
+                num_stack.push(int(i))
+            else:
+                if i in variables:
+                    print(variables)
+                    num_stack.push(variables[i])
+        if var_name_to_save == '':
+            print(num_stack.pop())
+        else:
+            variables[var_name_to_save] = num_stack.pop()
+            print(var_name_to_save)
     
