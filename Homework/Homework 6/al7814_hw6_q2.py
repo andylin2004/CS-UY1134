13a3093d53fa3c75fc6a7ea60729c447bd7b3355
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-16 22:47:19 -0500

    start q2

---
+++ Homework/Homework 6/al7814_hw6_q2.py
@@ -0,0 +1,9 @@
+class Integer:
+        def __init__(self, num_str):
+                ''' Initializes an Integer object representing
+                the value given in the string num_str'''
+        def __add__(self, other):
+                ''' Creates and returns an Integer object that represent the sum of self and other, also of type Integer'''
+        def __repr__(self):
+                ''' Creates and returns the string representation
+                of self'''
