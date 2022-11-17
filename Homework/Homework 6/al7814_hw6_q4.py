cf3befcd284dbab923db54f3847de6faea72d8f7
Andy Lin <andylin@nyu.edu>
2022-11-17 14:23:50 -0500

    deep copy works

--- Homework/Homework 6/al7814_hw6_q4.py
+++ Homework/Homework 6/al7814_hw6_q4.py
@@ -30,3 +30,20 @@ if __name__ == "__main__":
     e2 = lnk_lst2.header.next
     e2_1 = e2.data.header.next
     print(e2_1.data)
+
+    # deep copy
+
+    lnk_lst1 = DoublyLinkedList()
+    elem1 = DoublyLinkedList()
+    elem1.add_last(1)
+    elem1.add_last(2)
+    lnk_lst1.add_last(elem1)
+    elem2 = 3
+    lnk_lst1.add_last(elem2)
+    lnk_lst2 = deep_copy_linked_list(lnk_lst1)
+    e1 = lnk_lst1.header.next
+    e1_1 = e1.data.header.next
+    e1_1.data = 10
+    e2 = lnk_lst2.header.next
+    e2_1 = e2.data.header.next
+    print(e2_1.data)
