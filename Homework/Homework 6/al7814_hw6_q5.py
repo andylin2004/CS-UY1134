ab2a77ad0d032ae594d35ede675fe4f25288a5e9
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-17 16:42:44 -0500

    adjust code because things getting thrown into the list were kinda incorrect

--- Homework/Homework 6/al7814_hw6_q5.py
+++ Homework/Homework 6/al7814_hw6_q5.py
@@ -3,17 +3,35 @@ from DoublyLinkedList import *
 def merge_linked_lists(srt_lnk_lst1, srt_lnk_lst2):
     def merge_sublists(lst1_pointer, lst2_pointer, doubly_list):
         if lst1_pointer.data is not None and lst2_pointer.data is not None:
-            if lst1_pointer.data >= lst2_pointer.data:
+            if lst1_pointer.data <= lst2_pointer.data:
                 doubly_list.add_last(lst1_pointer.data)
-                merge_sublists(lst1_pointer.next, lst2_pointer, doubly_list)
+                return merge_sublists(lst1_pointer.next, lst2_pointer, doubly_list)
             else:
                 doubly_list.add_last(lst2_pointer.data)
-                merge_sublists(lst1_pointer, lst2_pointer.next, doubly_list)
+                return merge_sublists(lst1_pointer, lst2_pointer.next, doubly_list)
         elif lst1_pointer.data is not None:
             doubly_list.add_last(lst1_pointer.data)
-            merge_sublists(lst1_pointer.next, lst2_pointer, doubly_list)
+            return merge_sublists(lst1_pointer.next, lst2_pointer, doubly_list)
         elif lst2_pointer.data is not None:
             doubly_list.add_last(lst2_pointer.data)
-            merge_sublists(lst1_pointer, lst2_pointer.next, doubly_list)
+            return merge_sublists(lst1_pointer, lst2_pointer.next, doubly_list)
+        else:
+            return doubly_list 
 
     return merge_sublists(srt_lnk_lst1.header.next, srt_lnk_lst2.header.next, DoublyLinkedList())
+
+if __name__ == "__main__": 
+    srt_lnk_lst1 = DoublyLinkedList()
+    srt_lnk_lst1.add_last(1)
+    srt_lnk_lst1.add_last(3)
+    srt_lnk_lst1.add_last(5)
+    srt_lnk_lst1.add_last(6)
+    srt_lnk_lst1.add_last(8)
+    srt_lnk_lst2 = DoublyLinkedList()
+    srt_lnk_lst2.add_last(2)
+    srt_lnk_lst2.add_last(3)
+    srt_lnk_lst2.add_last(5)
+    srt_lnk_lst2.add_last(10)
+    srt_lnk_lst2.add_last(15)
+    srt_lnk_lst2.add_last(18)
+    print(merge_linked_lists(srt_lnk_lst1, srt_lnk_lst2))
