e7b7d05f95fbd3c2e5bd8f9954db442be595cf9a
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-16 22:35:32 -0500

    test code + more bug fix

--- Homework/Homework 6/al7814_hw6_q1.py
+++ Homework/Homework 6/al7814_hw6_q1.py
@@ -14,7 +14,26 @@ class LinkedQueue:
         return self.queue.delete_first()
     
     def first(self):
-        return self.queue.first()
+        return self.queue.header.next.data
 
     def is_empty(self):
         return self.queue.is_empty()
+
+if __name__ == "__main__": 
+    lq = LinkedQueue()
+    lq.enqueue(0)
+    lq.enqueue(1)
+    lq.enqueue(2)
+    lq.enqueue(3)
+    lq.enqueue(4)
+    lq.enqueue(5)
+    lq.enqueue(6)
+    lq.enqueue(7)
+    lq.enqueue(8)
+    lq.enqueue(9)
+    lq.dequeue()
+    lq.dequeue()
+    lq.dequeue()
+    lq.dequeue()
+    lq.dequeue()
+    print(lq.first())
