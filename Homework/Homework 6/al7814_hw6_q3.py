a47df7068c3c8242b731328ce950a8f48588c629
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-17 08:41:29 -0500

    add done

--- Homework/Homework 6/al7814_hw6_q3.py
+++ Homework/Homework 6/al7814_hw6_q3.py
@@ -4,20 +4,32 @@ class CompactString:
     def __init__(self, orig_str):
         ''' Initializes a CompactString object representing the string given in orig_str'''
         self.linked_list = DoublyLinkedList()
-        currently_repeating_char = None
-        total = 0
-        for i in orig_str:
-            if currently_repeating_char is not None:
-                if i != currently_repeating_char:
-                    self.linked_list.add_last((currently_repeating_char, total))
-                    total = 0
-            currently_repeating_char = i
-            total += 1
-        self.linked_list.add_last((currently_repeating_char, total))
+        if orig_str is not None:
+            currently_repeating_char = None
+            total = 0
+            for i in orig_str:
+                if currently_repeating_char is not None:
+                    if i != currently_repeating_char:
+                        self.linked_list.add_last((currently_repeating_char, total))
+                        total = 0
+                currently_repeating_char = i
+                total += 1
+            self.linked_list.add_last((currently_repeating_char, total))
 
     def __add__(self, other):
         ''' Creates and returns a CompactString object that represent the concatenation of self and other,
         also of type CompactString'''
+        new_compact = CompactString(None)
+        for i in self.linked_list:
+            new_compact.linked_list.add_last(i)
+        for i in other.linked_list:
+            if new_compact.linked_list.trailer.prev.data[0] == i[0]:
+                removed_from_last = new_compact.linked_list.delete_last()
+                new_compact.linked_list.add_last((removed_from_last[0], removed_from_last[1] + i[1]))
+            else:
+                new_compact.linked_list.add_last(i)
+        return new_compact
+
     def __lt__(self, other):
         ''' returns True if”f self is lexicographically less than other, also of type CompactString'''
     def __le__(self, other):
@@ -32,3 +44,6 @@ class CompactString:
 if __name__ == "__main__": 
     compact_string = CompactString("aaaaabbbaaac")
     print(compact_string.linked_list)
+    compact_2 = CompactString("aaaaaaacccaaaa")
+    compact_sum = compact_2 + compact_string
+    print(compact_sum.linked_list)
