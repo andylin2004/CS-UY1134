3ee23be11bb94e2d86c52c7c1c646026d638526c
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-16 23:17:19 -0500

    integer class works now

--- Homework/Homework 6/al7814_hw6_q2.py
+++ Homework/Homework 6/al7814_hw6_q2.py
@@ -5,12 +5,30 @@ class Integer:
         ''' Initializes an Integer object representing
         the value given in the string num_str'''
         self.linked_list = DoublyLinkedList()
-        for char in num_str:
-            self.linked_list.add_last(int(char))
+        if num_str is not None:
+            for char in num_str:
+                self.linked_list.add_last(int(char))
 
     def __add__(self, other):
         ''' Creates and returns an Integer object that represent the sum of self and other, also of type Integer'''
-    
+        self_cursor = self.linked_list.trailer.prev
+        other_cursor = other.linked_list.trailer.prev
+        overflow = 0
+        new_integer = Integer(None)
+        while self_cursor.data is not None or other_cursor.data is not None:
+            total = overflow
+            if self_cursor.data is not None:
+                total += self_cursor.data
+            if other_cursor.data is not None:
+                total += other_cursor.data
+            new_integer.linked_list.add_first(total % 10)
+            overflow = total // 10
+            if self_cursor.data is not None:
+                self_cursor = self_cursor.prev
+            if other_cursor.data is not None:
+                other_cursor = other_cursor.prev
+        return new_integer
+
     def __repr__(self):
         ''' Creates and returns the string representation
         of self'''
@@ -18,4 +36,7 @@ class Integer:
 
 if __name__ == "__main__": 
     integer = Integer("375")
+    integer2 = Integer("4029")
     print(integer)
+    integer3 = integer + integer2
+    print(integer3)
