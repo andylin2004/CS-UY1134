8833de9d2a95e92f8d178a0e606f14d7d2754f49
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-16 22:26:19 -0500

    added most of queue adt stuff

--- Homework/Homework 6/al7814_hw6_q1.py
+++ Homework/Homework 6/al7814_hw6_q1.py
@@ -3,3 +3,18 @@ from DoublyLinkedList import *
 class LinkedQueue:
     def __init__(self) -> None:
         self.queue = DoublyLinkedList()
+
+    def __len__(self):
+        return len(self.queue)
+    
+    def enqueue(self, value):
+        self.queue.add_last(value)
+    
+    def dequeue(self):
+        return self.queue.delete_first()
+    
+    def first(self):
+        return self.queue.header
+
+    def is_empty(self):
+        return self.queue.is_empty()
