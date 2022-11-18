af4d2f11f2b4ca0971f890c72dbf71ffe21c9f5d
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-18 09:40:22 -0500

    q2 done

--- Labs/Lab9.py
+++ Labs/Lab9.py
@@ -1,10 +1,12 @@
 from ArrayQueue import *
 
+# q1
+
 class MeanQueue:
     def __init__(self):
         self.data = ArrayQueue()
         self.total = 0
-        
+
     def __len__(self):
         '''Return the number of elements in the queue'''
         return len(self.data)
@@ -44,3 +46,33 @@ class MeanQueue:
     def mean(self):
         ''' Return the mean (average) value in the queue'''
         return self.total / len(self.data)
+
+# q2
+
+def flatten_list_by_depth(lst): 
+    """
+    : lst type: list
+    : return type: list
+    """
+    q = ArrayQueue()
+    new_lst = []
+    ''' Remove and return the top element from the stack. If the
+    stack is empty, raise an exception'''
+    for i in lst:
+        q.enqueue(i)
+    while not q.is_empty():
+        if isinstance(q.first(), list):
+            total = len(q) - 1
+            for i in q.dequeue():
+                q.enqueue(i)
+            for _ in range(total):
+                q.enqueue(q.dequeue())
+        else:
+            new_lst.append(q.dequeue())
+    return new_lst
+
+
+if __name__ == "__main__": 
+    lst = [ [[[0]]], [1, 2], 3, [4, [5, 6, [7]], 8], 9]
+    new_lst = flatten_list_by_depth(lst)
+    print(new_lst)
