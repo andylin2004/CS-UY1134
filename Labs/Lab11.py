cd96bc428ceca9f2e82eabf6c28867ea97f27197
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-20 16:16:34 -0500

    added bt_contains

--- Labs/Lab11.py
+++ Labs/Lab11.py
@@ -10,3 +10,13 @@ def bt_even_sum(root):
         if root.data % 2 == 0:
             total += root.data
         return total + bt_even_sum(root.left) + bt_even_sum(root.right)
+
+def bt_contains(root, val):
+    ''' Returns True if val exists in the binary tree and
+    false if not'''
+    if root is None:
+        return False
+    else:
+        return root.data == val or bt_contains(root.left, val) or bt_contains(root.right, val)
+
+        
