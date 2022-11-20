ef3769043dccc883d8f6ba793cea153a988366de
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-20 16:31:07 -0500

    test cases made

--- Labs/Lab11.py
+++ Labs/Lab11.py
@@ -19,4 +19,15 @@ def bt_contains(root, val):
     else:
         return root.data == val or bt_contains(root.left, val) or bt_contains(root.right, val)
 
-        
+if __name__ == "__main__": 
+    n1 = LinkedBinaryTree.Node(1)
+    n2 = LinkedBinaryTree.Node(2)
+    n12 = LinkedBinaryTree.Node(3, n1, n2)
+    n3 = LinkedBinaryTree.Node(4)
+    n4 = LinkedBinaryTree.Node(5)
+    n34 = LinkedBinaryTree.Node(6, n3, n4)
+    n_apex = LinkedBinaryTree.Node(7, n12, n34)
+    bt = LinkedBinaryTree(n_apex)
+    print(bt_contains(n_apex, 0))
+    print(bt_contains(n_apex, 1))
+    print(bt_even_sum(n_apex))
