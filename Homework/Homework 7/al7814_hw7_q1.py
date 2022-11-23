a3a9b67337e078b6eb2c095e6255833fdbb9a70b
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-23 18:08:08 -0500

    fixed code not getting the actual node value

--- Homework/Homework 7/al7814_hw7_q1.py
+++ Homework/Homework 7/al7814_hw7_q1.py
@@ -7,13 +7,19 @@ def min_and_max(bin_tree):
         else:
             left_minmax = subtree_min_and_max(root.left)
             right_minmax = subtree_min_and_max(root.right)
-            if left_minmax is None:
-                return right_minmax
+            if left_minmax is None and right_minmax is None:
+                return (root.data, root.data)
+            elif left_minmax is None:
+                grand_min = min(root.data, right_minmax[0])
+                grand_max = max(root.data, right_minmax[1])
+                return (grand_min, grand_max)
             elif right_minmax is None:
-                return left_minmax
+                grand_min = min(root.data, left_minmax[0])
+                grand_max = max(root.data, left_minmax[1])
+                return (grand_min, grand_max)
             else:
-                grand_min = min(left_minmax[0], right_minmax[0])
-                grand_max = max(left_minmax[1], right_minmax[1])
+                grand_min = min(left_minmax[0], right_minmax[0], root.data)
+                grand_max = max(left_minmax[1], right_minmax[1], root.data)
                 return (grand_min, grand_max)
 
     return subtree_min_and_max(bin_tree.root)
