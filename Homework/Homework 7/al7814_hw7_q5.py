2dfa35e7a9872197d090b2d56835dd7674db5876
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-24 11:15:03 -0500

    q5 part a done

--- Homework/Homework 7/al7814_hw7_q5.py
+++ Homework/Homework 7/al7814_hw7_q5.py
@@ -21,4 +21,17 @@ def create_expression_tree(prefix_exp_str):
             start_pos = subexp_result[1]
         return (node, start_pos)
 
-    return create_expression_tree_helper(0)[0]
+    return LinkedBinaryTree(create_expression_tree_helper(0)[0])
+
+if __name__ == "__main__": 
+    exp_tree = create_expression_tree('* 2 + - 15 6 4')
+    for i in exp_tree.preorder():
+        print(i.data, end=' ')
+    print()
+    exp_tree = create_expression_tree('- * 3 4 10')
+    for i in exp_tree.preorder():
+        print(i.data, end=' ')
+    print()
+    exp_tree = create_expression_tree('+ * 6 3 * 8 4')
+    for i in exp_tree.preorder():
+        print(i.data, end=' ')
