1c055bc58dfeafb196a20cab2618c2b57cb46999
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-02 09:44:29 -0500

    q2 lab12 done

--- Labs/Lab12.py
+++ Labs/Lab12.py
@@ -1,3 +1,4 @@
+#q1
 def min_max_BST(bst):
     ''' Returns a tuple containing the min and max keys in the
     binary search tree'''
@@ -8,3 +9,15 @@ def min_max_BST(bst):
     while right.right is not None:
         right = right.right
     return (left.item.key, right.item.key)
+
+#q2
+def glt_n(bst, n): #glt = greatest less than
+    ''' Returns the greatest number in the binary search tree
+    less than or equal to n'''
+    node = bst.root
+    while bst.root is not None and ((bst.root.item.key < n and bst.root.right is not None) or (bst.root.item.key > n and bst.root.left is not None)):
+        if bst.root.item.key < n:
+            node = node.right
+        elif bst.root.item.key > n:
+            node = node.left
+    return node
