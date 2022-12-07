4950ee38c977f8f77f6bf01669d05f1aa66810f6
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-06 23:35:42 -0500

    adjust how code is structured

--- Homework/Homework 8/al7814_hw8_q2.py
+++ Homework/Homework 8/al7814_hw8_q2.py
@@ -5,14 +5,14 @@ def create_chain_bst(n):
     for i in range(1, n+1):
         bst.insert(i, i)
 
+def add_items(bst, low, high):
+    mid = (high + low) // 2
+    bst.insert(mid, mid)
+    if high-low > 1:
+        add_items(bst, low, mid - 1)
+        add_items(bst, mid + 1, high)
+        
 def create_complete_bst(n):
-    def add_items(bst, low, high):
-        mid = (high + low) // 2
-        bst.insert(mid, mid)
-        if high-low > 1:
-            add_items(bst, low, mid - 1)
-            add_items(bst, mid + 1, high)
-
     bst = BinarySearchTreeMap()
     add_items(bst, 1, n)
     return bst
