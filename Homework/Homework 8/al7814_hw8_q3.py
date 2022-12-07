71d8dc4d48147b1d86a7cc76b3625c8bc34d0f48
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-07 16:18:37 -0500

    q3 kinda done?

---
+++ Homework/Homework 8/al7814_hw8_q3.py
@@ -0,0 +1,32 @@
+from BinarySearchTreeMap import *
+
+def restore_bst(prefix_lst):
+    bst = BinarySearchTreeMap()
+
+    def recursive_part(node, cur_indice):
+        if prefix_lst[cur_indice] < node.item.key:
+            new_node = BinarySearchTreeMap.Node(BinarySearchTreeMap.Item(prefix_lst[cur_indice], prefix_lst[cur_indice]))
+            node.left = new_node
+            if cur_indice + 1 < len(prefix_lst):
+                if prefix_lst[cur_indice] < prefix_lst[0]:
+                    if prefix_lst[cur_indice + 1] < prefix_lst[0]:
+                        if prefix_lst[cur_indice + 1] > node.item.key: 
+                            recursive_part(node, cur_indice + 1)
+                        else:
+                            recursive_part(node.left, cur_indice + 1)
+                    else:
+                        recursive_part(bst.root, cur_indice + 1)
+        else:
+            new_node = BinarySearchTreeMap.Node(BinarySearchTreeMap.Item(prefix_lst[cur_indice], prefix_lst[cur_indice]))
+            node.right = new_node
+            if cur_indice + 1 < len(prefix_lst):
+                if prefix_lst[cur_indice] < prefix_lst[0]:
+                    if prefix_lst[cur_indice + 1] < node.item.key: 
+                        recursive_part(node, cur_indice + 1)
+                    else:
+                        recursive_part(node.right, cur_indice + 1)
+    
+    if len(prefix_lst) > 1:
+        bst.insert(prefix_lst[0], prefix_lst[0])
+        recursive_part(bst.root, 1)
+        
