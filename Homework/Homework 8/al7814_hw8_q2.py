ac0d9afed6587c175b029041f82a9e319e62236e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-06 23:30:08 -0500

    q2 done with fixed binary tree?

--- Homework/Homework 8/al7814_hw8_q2.py
+++ Homework/Homework 8/al7814_hw8_q2.py
@@ -1,6 +1,22 @@
 from BinarySearchTreeMap import *
 
 def create_chain_bst(n):
-    bst = BinartSearchTreeMap()
+    bst = BinarySearchTreeMap()
     for i in range(1, n+1):
         bst.insert(i, i)
+
+def create_complete_bst(n):
+    def add_items(bst, low, high):
+        mid = (high + low) // 2
+        print(low, mid, high)
+        bst.insert(mid, mid)
+        if high-low > 1:
+            add_items(bst, low, mid - 1)
+            add_items(bst, mid + 1, high)
+
+    bst = BinarySearchTreeMap()
+    add_items(bst, 1, n)
+    return bst
+
+if __name__ == "__main__": 
+    print(create_complete_bst(7))
