0eaa968198d24ddde4f9058434a921ed3d0786a1
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-07 21:23:50 -0500

    seems like it should be working now

--- Homework/Homework 8/al7814_hw8_q3.py
+++ Homework/Homework 8/al7814_hw8_q3.py
@@ -4,26 +4,29 @@ def restore_bst(prefix_lst):
     bst = BinarySearchTreeMap()
 
     def recursive_part(node, cur_indice):
+        print(node.item.key, prefix_lst[cur_indice])
         if prefix_lst[cur_indice] < node.item.key:
-            new_node = BinarySearchTreeMap.Node(BinarySearchTreeMap.Item(prefix_lst[cur_indice], prefix_lst[cur_indice]))
+            print("l")
+            new_node = BinarySearchTreeMap.Node(BinarySearchTreeMap.Item(prefix_lst[cur_indice]))
             node.left = new_node
             if cur_indice + 1 < len(prefix_lst):
-                if prefix_lst[cur_indice + 1] < prefix_lst[0]:
-                    if prefix_lst[cur_indice + 1] > node.item.key: 
-                        recursive_part(node, cur_indice + 1)
-                    else:
-                        recursive_part(node.left, cur_indice + 1)
-                else:
+                if prefix_lst[cur_indice + 1] > prefix_lst[0] and prefix_lst[cur_indice] < prefix_lst[0]:
                     recursive_part(bst.root, cur_indice + 1)
+                elif prefix_lst[cur_indice + 1] < prefix_lst[cur_indice]:
+                    recursive_part(node.left, cur_indice + 1)
+                else:
+                    recursive_part(node, cur_indice + 1)
         else:
-            new_node = BinarySearchTreeMap.Node(BinarySearchTreeMap.Item(prefix_lst[cur_indice], prefix_lst[cur_indice]))
+            print("r")
+            new_node = BinarySearchTreeMap.Node(BinarySearchTreeMap.Item(prefix_lst[cur_indice]))
             node.right = new_node
             if cur_indice + 1 < len(prefix_lst):
-                if prefix_lst[cur_indice + 1] > prefix_lst[0]:
-                    if prefix_lst[cur_indice + 1] < node.item.key: 
-                        recursive_part(node, cur_indice + 1)
-                    else:
-                        recursive_part(node.right, cur_indice + 1)
+                if prefix_lst[cur_indice + 1] > prefix_lst[0] and prefix_lst[cur_indice] < prefix_lst[0]:
+                    recursive_part(bst.root, cur_indice + 1)
+                elif prefix_lst[cur_indice + 1] > prefix_lst[cur_indice] or node.left is not None:
+                    recursive_part(node.right, cur_indice + 1)
+                else:
+                    recursive_part(node, cur_indice + 1)
     
     if len(prefix_lst) > 1:
         bst.insert(prefix_lst[0], prefix_lst[0])
