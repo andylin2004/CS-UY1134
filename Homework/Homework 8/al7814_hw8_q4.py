89f5d3e7ebdf52c90f08f531c270da9ee599f2c9
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-07 23:16:06 -0500

    should bubble up the middle one now

--- Homework/Homework 8/al7814_hw8_q4.py
+++ Homework/Homework 8/al7814_hw8_q4.py
@@ -2,23 +2,49 @@ from BinarySearchTreeMap import *
 
 def find_min_abs_difference(bst):
     def recursive_part(node, is_left):
-        if node.left is None and node.right is None:
-            return (node.item.key)
+        if node is None:
+            return None
         else:
             left_side = recursive_part(node.left, True)
             right_side = recursive_part(node.right, False)
-            left_diff = abs(node.item.key - left_side[0])
-            right_diff = abs(node.item.key - right_side[0])
-            min_dif = min(left_diff, right_diff)
-            if is_left:
+            min_dif = None
+            if left_side is not None:
+                left_diff = abs(node.item.key - left_side[0])
+                min_dif = left_diff
+                if left_side[1] is not None:
+                    min_dif = min(min_dif, left_side[1])
+            if right_side is not None:
+                right_diff = abs(node.item.key - right_side[0])
+                if min_dif is None:
+                    min_dif = right_diff
+                else:
+                    min_dif = min(min_dif, right_diff)
+                if right_side[1] is not None:
+                    min_dif = min(min_dif, right_side[1])
+            to_store = None
+            if is_left and right_side is not None:
                 to_store = right_side[0]
-                if len(right_side) >= 3:
+                if right_side[2] is not None:
                     stored_diff = abs(node.item.key - right_side[2])
                     min_dif = min(min_dif, stored_diff)
-            else:
+            elif left_side is not None:
                 to_store = left_side[0]
-                if len(left_side) >= 3:
+                if left_side[2] is not None:
                     stored_diff = abs(node.item.key - left_side[2])
                     min_dif = min(min_dif, stored_diff)
             return (node.item.key, min_dif, to_store)
             
+    return recursive_part(bst.root, False)[1]
+
+if __name__ == "__main__": 
+    bst = BinarySearchTreeMap()
+    bst.insert(9,1)
+    bst.insert(7,1)
+    bst.insert(20,1)
+    bst.insert(4,1)
+    bst.insert(1,1)
+    bst.insert(6,1)
+    bst.insert(17,1)
+    bst.insert(25,1)
+    print([x.item.key for x in bst.inorder()])
+    print(find_min_abs_difference(bst))
